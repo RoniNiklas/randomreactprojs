b69ede900119bda0fd4007f0ccc19f82464137f1
Roni <bibburi@hotmail.com>
2020-02-21 17:24:18 +0200

    50km range so that you can get in from Kerava and Siuntio

--- client/src/Eficode/services/hslApi.js
+++ client/src/Eficode/services/hslApi.js
@@ -54,7 +54,7 @@ const fetchItineraries = async (lat, lon) => {
 }
 
 const getLocationCoordinates = async (text) => {
-    const response = await axios.get("https://api.digitransit.fi/geocoding/v1/search?text=" + text + "&boundary.circle.lat=60.169447&boundary.circle.lon=24.925796&boundary.circle.radius=30")
+    const response = await axios.get("https://api.digitransit.fi/geocoding/v1/search?text=" + text + "&boundary.circle.lat=60.169447&boundary.circle.lon=24.925796&boundary.circle.radius=50")
     const coordinates = response.data.features[0].geometry.coordinates
     return {coords:{latitude: coordinates[1], longitude:coordinates[0]}}
 }
